05870227a4d4b02f6469956088ccedba7dd7dabf
Hiroaki Nishimine <n.aki.28.inov@gmail.com>
2025-05-05 14:39:33 +0900

    gitignoreを作成

---
+++ AntiSnoozeWatch Watch App/AntiSnoozeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntiSnoozeWatchApp.swift
+//  AntiSnoozeWatch Watch App
+//
+//  Created by 西峯弘晃 on 2025/05/05.
+//
+
+import SwiftUI
+
+@main
+struct AntiSnoozeWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AntiSnoozeWatch Watch AppTests/AntiSnoozeWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AntiSnoozeWatch_Watch_AppTests.swift
+//  AntiSnoozeWatch Watch AppTests
+//
+//  Created by 西峯弘晃 on 2025/05/05.
+//
+
+import Testing
+@testable import AntiSnoozeWatch_Watch_App
+
+struct AntiSnoozeWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
